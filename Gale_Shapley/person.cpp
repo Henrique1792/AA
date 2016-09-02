b235af484373a01e63e59844e0d43033b56e47af
Henrique1792 <freitash1792@gmail.com>
2016-09-01 22:49:43 -0300

    Logic fixed, .gitignore added

--- Gale_Shapley/person.cpp
+++ Gale_Shapley/person.cpp
@@ -1,30 +1,44 @@
 #include "person.h"
-#include <iostream>
+using namespace std;
 
-
-//classes constructors
-Person::Person(char ID){
+//class constructors
+Person::Person(char ID, char gender){
   this->ID=ID;
   this->set_marriage(false);
-};
+  this->gender=gender;
+}
 
 
 //class destructor
-Person::~Person(){};
+Person::~Person(){}
 
 //set marriage
 void Person::set_marriage(bool value){
   this->Married=value;
-};
+}
 
 //getters
 bool Person::get_mariage(){
   return this->Married;
-};
+}
 
 char Person::get_id(){
   return this->ID;
-};
+}
+char Person::get_gender(){
+  return this->gender;
 
-
-Man::Man(char ID):Person(ID){}
+}
+void Person::set_priorities(unsigned int people){
+  char reading;
+  for (this->priority_check=this->priority.begin();
+  this->priority.size()<people;
+  this->priority_check++){
+      cin>>reading;
+      this->priority.push_back(reading);
+  }
+  this->priority_check=this->priority.begin();
+}
+std::vector<char> Person::get_priorities(){
+  return this->priority;
+}
--- Gale_Shapley/person.h
+++ Gale_Shapley/person.h
@@ -1,30 +1,27 @@
 #ifndef _PERSON_H_
 #define _PERSON_H_
 #include <iostream>
-
+#include <vector>
+#include <iterator>
 
 class Person{
+  protected:
     char ID;
     bool Married;
-    char *priority; 
-
+    std::vector <char> priority;
+    std::vector<char>::iterator  priority_check;
+    char Married_With;
+    char gender;
+ 
   public:
-    Person(char ID);
+    Person(char ID, char gender);
     ~Person();
     void set_marriage(bool value);
-    void set_priorities();
-    char *get_priorities();
+    void set_priorities(unsigned int people);
+    char get_gender(); 
+    std::vector <char> get_priorities();
     bool  get_mariage();
     char get_id();
-};
-
-class Man : public Person{
-
-  public:
-    Man(char ID);
-    ~Man();
-
-
 };
 
 
