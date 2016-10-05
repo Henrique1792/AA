78d1e8cc83fe3dd78c2b5f4414b62093a81c1cb6
Henrique1792 <freitash1792@gmail.com>
2016-10-05 13:33:12 -0300

    Inequation detached

---
+++ Futoshiki/inequation.c
@@ -0,0 +1,24 @@
+#include "inequation.h"
+INEQUATION *create_inq(){
+  INEQUATION *rt=(INEQUATION *)malloc(sizeof(INEQUATION));
+  return rt==NULL ?  NULL : rt;
+}
+
+int set_inq_values(INEQUATION *tgt, int x1, int x2, int y1, int y2, int size){
+  if((x1>=size ||x1<0)||
+    (x2>=size ||x2<0)||
+    (y1>=size ||y1<0)||
+    (y2>=size ||y2<0)|| 
+    tgt==NULL) return FAIL;
+
+  tgt->x1=x1;  
+  tgt->x2=x2;  
+  tgt->y1=y1;  
+  tgt->y2=x2; 
+  return SUCCESS;
+}
+
+void free_inq(INEQUATION *tgt){
+  if(tgt==NULL) return;
+  free(tgt);
+}
---
+++ Futoshiki/inequation.h
@@ -0,0 +1,26 @@
+#ifndef INEQUATION_H_
+#define INEQUATION_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define SUCCESS 1
+#define FAIL 0
+#define DEFAULT -1
+
+
+typedef struct INEQUATION{
+  int x1, x2; //point A
+  int y1, y2; //point B
+}INEQUATION;
+
+INEQUATION *create_inq();
+int set_inq_values(INEQUATION *tgt,int x1, int x2, int y1, int y2, int size);
+void free_inq(INEQUATION *tgt);
+
+
+
+
+
+#endif
