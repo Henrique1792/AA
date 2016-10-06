c5891ebf0e4b5dc57f5adf71268baac301a018f4
Henrique1792 <freitash1792@gmail.com>
2016-10-06 13:28:25 -0300

    Set up comparations

--- Futoshiki/rules.c
+++ Futoshiki/rules.c
@@ -1,37 +1,70 @@
 #include "rules.h"
 
-/*Não gostei dessa opção...
- * não sei como serão as chamadas da função
- * recursiva acima, pode gerar muito
- * tempo de execução.
-*/
+
+
+int compare_vectors(int *v1, int *v2, int size){
+  int k;
+  
+
+  for(k=0;k<size;k++){
+    if(v1[k]!=v2[k]){
+      return FAIL;
+    }
+  }
+  return SUCCESS; 
+}
+
+
+
+
 int check_lines(GAME *tgt){
   if(tgt==NULL)  return FAIL;
-  int i,j,k,*chk;
+  int i, j, result,*chk;
 
 
   for(i=0;i<tgt->size;i++){
     for(j=0;j<tgt->size;j++){
-      if(tgt->table[i][j]==DEFAULT) break;
-    }
+      if(tgt->table[i][j]==DEFAULT){
+        result=FAIL; 
+        break;
+      }
+    }      
+    
+    if(result==FAIL) break;
     memcpy(chk,tgt->table[i],sizeof(tgt->table[i]));
     quicksort(chk,0,(tgt->size)-1);
-
-    for(k=0;k<tgt->size;k++){
-      if(tgt->possible_values[k]!=chk[k]){
-        free(chk);
-        return FAIL;
-      }
-      free(chk);
-    }
+    result=compare_vectors(chk,tgt->possible_values,tgt->size);
+    free(chk);
+    if(result==FAIL)  break;
+    
   }
   return SUCCESS;
 }
 
 int check_columns(GAME *tgt){
-  if(tgt==NULL) return FAIL;
+  if(tgt==NULL)  return FAIL;
+  int i, j, result,*chk;
 
+  
+  for(i=0;i<tgt->size;i++){
+    for(j=0;j<tgt->size;j++){    
+      if(tgt->table[j][i]==DEFAULT){
+        result=FAIL;
+        break;
+      }
+    }
 
+    if(result==FAIL) break;
+    chk=(int *)malloc(tgt->size*sizeof(int));
+    for(j=0;j<tgt->size;j++) chk[j]=tgt->table[j][i];
+    
+    quicksort(chk,0,(tgt->size)-1);
+    result=compare_vectors(chk,tgt->possible_values,tgt->size);
+    free(chk);
+    if(result==FAIL) break;
+  }
+  return result;
+  
 }
 
 int check_inequations(GAME *tgt){
